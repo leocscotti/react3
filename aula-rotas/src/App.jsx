20fd1fc1e86faba25456354bcf15007e1a800726
leocscotti <leoscotti03@gmail.com>
2023-09-13 18:11:58 -0300

    menu e rodapé configurados

--- aula-rotas/src/App.jsx
+++ aula-rotas/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Menu/>    
       <Outlet/>
+      <Rodape/>
     </>
   )
 }
