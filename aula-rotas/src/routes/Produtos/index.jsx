25ecfd9f61cdd3b809126629ad67794ffb1285c4
leocscotti <leoscotti03@gmail.com>
2023-09-13 18:25:37 -0300

    lista de produtos importada em Produtos

--- aula-rotas/src/routes/Produtos/index.jsx
+++ aula-rotas/src/routes/Produtos/index.jsx
@@ -1,8 +1,12 @@
+import {Link} from "react-router-dom"
+import { listaProdutos } from "../../components/listaProdutos"
+
 export default function Produtos(){
     
     return(
         <main>
             <h1>Produtos</h1>
+            
         </main>
     )
 }
