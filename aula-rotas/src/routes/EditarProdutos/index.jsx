17c1b5a908bb9099a06694cbd04955e07322a78f
leocscotti <leoscotti03@gmail.com>
2023-09-13 18:39:02 -0300

    useParams e useNavigate utilizados edição dos produtos adicionada e habilitada

--- aula-rotas/src/routes/EditarProdutos/index.jsx
+++ aula-rotas/src/routes/EditarProdutos/index.jsx
@@ -1,11 +1,25 @@
-import { useParms, useNavigate } from "react-router-dom"
+import { useParms, useNavigate, useParams } from "react-router-dom"
 import { listaProdutos } from "../../components/listaProdutos"
 
 export default function EditarProdutos(){
     
+    const lista= listaProdutos
+    const navegacao= useNavigate()
+    const{id}= useParams
+
+    const proc= lista.filter(prod=> prod.id== id)
+    const produto= proc[0]
+
+    const salvar= ()=>{
+        alert(`Produto: ${produto.nome} editado com sucesso!`)
+        return navegacao ('/produtos')
+    }
+
     return(
         <main>
-            <h1>Editando produto</h1>
+            <h1>Editando o produto</h1>
+            <p>Editando os dados do produto: {produto.nome}</p>
+            <button onClick= {salvar}>Salvar</button>
         </main>
     )
 }
